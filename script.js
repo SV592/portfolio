f538a8404711883265a8203c11640d3ee1f31754
Shaquille Pearson <shaquillepearson9@gmail.com>
2024-04-08 12:58:26 -0400

    removed experience buttons

--- script.js
+++ script.js
@@ -1,16 +1,8 @@
-function fuck() {
-    document
-        .querySelectorAll(".experience-item .toggle-content")
-        .forEach(function (button) {
-            button.addEventListener("click", function () {
-                var list = this.previousElementSibling;
-                if (list.style.display === "none" || list.style.display === "") {
-                    list.style.display = "block";
-                    this.textContent = "View Less";
-                } else {
-                    list.style.display = "none";
-                    this.textContent = "View More";
-                }
-            });
-        });
+function changeDisplay() {
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
 }
