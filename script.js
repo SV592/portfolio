9c57b5dec0cec1744154bd1a56d094a5ff53606b
Shaquille Pearson <shaquillepearson9@gmail.com>
2024-04-15 17:24:10 -0400

    Adjusted all button sizes

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-function changeDisplay() {
-    var x = document.getElementById("myDIV");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
-    }
-}
+window.addEventListener('DOMContentLoaded', (event) => {
+    document.getElementById('theme-toggle').addEventListener('click', function () {
+        this.classList.toggle('dark-theme');
+        this.classList.toggle('light-theme');
+        document.getElementById('sun-icon').style.display = this.classList.contains('dark-theme') ? 'none' : 'inline';
+        document.getElementById('moon-icon').style.display = this.classList.contains('dark-theme') ? 'inline' : 'none';
+    });
+});
