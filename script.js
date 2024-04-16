8829721f81b2d44b3428f986b9dffb165b7f5888
Shaquille Pearson <shaquillepearson9@gmail.com>
2024-04-16 13:07:00 -0400

    Added margin to section titles

--- script.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('DOMContentLoaded', (event) => {
-    document.getElementById('theme-toggle').addEventListener('click', function () {
-        this.classList.toggle('dark-theme');
-        this.classList.toggle('light-theme');
-        document.getElementById('sun-icon').style.display = this.classList.contains('dark-theme') ? 'none' : 'inline';
-        document.getElementById('moon-icon').style.display = this.classList.contains('dark-theme') ? 'inline' : 'none';
-    });
-});
